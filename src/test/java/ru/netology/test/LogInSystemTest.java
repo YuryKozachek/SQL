fac48652a81a5b25e5f5f51bab802748dc56825f
Kozachek Yury <show_@mail.ru>
2023-04-26 23:23:24 +0300

    initial commit

---
+++ src/test/java/ru/netology/test/LogInSystemTest.java
@@ -0,0 +1,32 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.DataSQL;
+import ru.netology.page.LoginPage;
+import ru.netology.page.VerificationPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.DataSQL.cleanDataBase;
+
+public class LogInSystemTest {
+
+
+    @AfterAll
+    static void cleanDB() {
+        cleanDataBase();
+    }
+
+    LoginPage loginPage;
+
+    @Test
+    void shouldLogInSystem() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+        DataHelper.AuthInfo authInfo = DataHelper.getAuthInfo();
+        VerificationPage verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verificationPageVisible();
+        DataHelper.VerificationCode verificationCode = DataSQL.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+}
